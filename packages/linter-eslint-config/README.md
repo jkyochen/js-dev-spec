710beac501890ed8c15b90ae41153856987de9be
Lanlyhs <ziqiang.chen@outlook.com>
2022-02-27 21:26:36 +0800

    Add code linter rule

---
+++ packages/linter-eslint-config/README.md
@@ -0,0 +1,15 @@
+# `linter-eslint-config`
+
+## Usage
+
+```sh
+# 1. Install package
+npm install --save-dev eslint @lanlyhs/linter-eslint-config
+
+# 2. Add to .eslintrc
+{
+    "extends": [
+        "@lanlyhs/linter-eslint-config"
+    ]
+}
+```
---
+++ packages/linter-eslint-config/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:node/recommended",
+        "plugin:unicorn/recommended",
+        "plugin:sonarjs/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "rules": {
+        "eqeqeq": 2,
+        "require-await": 2,
+        "complexity": [
+            2,
+            15
+        ],
+        "max-lines-per-function": [
+            2,
+            20
+        ]
+    }
+}
